f651034203490c6feb2024bc04c61f44e63dc39f
virikidor-oss <virikidor@gmail.com>
2026-07-13 01:23:33 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,12 @@ const basePath =
   process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : "";
 
 const nextConfig: NextConfig = {
-  
-
   ...(basePath && {
     basePath,
     assetPrefix: basePath,
   }),
 
   images: {
-    
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
